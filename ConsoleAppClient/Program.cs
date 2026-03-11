85cb4a357280e741aa7f0cba4508e49fd12733d0
tuomas <tuomas.lattila@outlook.com>
2026-03-11 13:38:39 +0200

    Added server and client projects. Implemented gRPC service on the server side "AddNewNote" which receives the new note through gRPC from client and sends response. Client currently only send one new note as test. The gRPC connection works.

---
+++ ConsoleAppClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+using ConsoleAppClient;
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        // The port number must match the port of the gRPC server.
+        using var channel = GrpcChannel.ForAddress("https://localhost:7101");
+        var client = new NoteService.NoteServiceClient(channel);
+        var reply = await client.CreateNoteAsync(
+            new NoteRequest { Topic = "Test topic: Animals", Text = "Animals are funny", Timestamp = DateTime.UtcNow.ToString("MM/dd/yy - HH:mm:ss")});
+        Console.WriteLine(reply.Message);
+        Console.WriteLine("Press any key to exit....");
+        Console.ReadLine();
+    }
+}
---
+++ GrpcNotebookService/Services/NoteService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;    
+
+namespace GrpcNotebookService.Services
+{
+    public class AddNewNote(ILogger<AddNewNote> logger) : NoteService.NoteServiceBase
+    {
+        public override Task<NoteReply> CreateNote(NoteRequest request, ServerCallContext context)
+        {
+            logger.LogInformation($"The note is received with topic of {request.Topic}");
+            return Task.FromResult(new NoteReply
+            {
+                Message = $"The note with topic of {request.Topic} is received successfully"
+            });
+        }
+    }
+}
